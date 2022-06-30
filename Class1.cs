f261f875cf703b9c3adf31662aee77306e632726
Aaron Sherber <aaron@sherber.com>
2022-06-30 19:33:04 -0400

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using Foo.Logging;
+
+namespace Foo
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            var log = LogProvider.For<Class1>();
+            log.Info("Test");
+        }
+    }
+}
